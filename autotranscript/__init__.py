724c2844741e8e976e3fa1978b2f102112125c76
Jaikinator <schmieder.jacob@web.de>
2023-06-09 18:00:46 +0200

    added files to module init

--- autotranscript/__init__.py
+++ autotranscript/__init__.py
@@ -1,6 +1,7 @@
 from autotranscript.__main__ import *
 from autotranscript.transcriptor import *
 from autotranscript.audio_processor import *
+from autotranscript.diarisation import *
 from autotranscript.version import get_version as _get_version
 from autotranscript.misc import *
 
