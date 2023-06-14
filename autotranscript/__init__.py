34354c055f7514cad065b9e00a7273308a138657
Jaikinator <schmieder.jacob@web.de>
2023-06-14 16:30:29 +0200

    changed imports

--- autotranscript/__init__.py
+++ autotranscript/__init__.py
@@ -1,6 +1,7 @@
 from autotranscript.__main__ import *
 from autotranscript.transcriber import *
-from autotranscript.audio_processor import *
+from autotranscript.audio import *
+from autotranscript.transcript_exporter import *
 from autotranscript.diarisation import *
 from autotranscript.version import get_version as _get_version
 from autotranscript.misc import *
