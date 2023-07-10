52754c988552f7ac45013f49ab1963f94ced5e78
Jaikinator <schmieder.jacob@web.de>
2023-07-10 13:29:09 +0200

    removed unnecessary stuff

--- autotranscript/misc.py
+++ autotranscript/misc.py
@@ -1,8 +1,4 @@
-from pyannote.audio import Pipeline
-from whisper import Whisper, load_model
 import os
-import glob
-from warnings import warn
 import yaml
 
 CACHE_DIR = os.getenv(
@@ -32,22 +28,8 @@ def config_diarization_yaml(file, path_to_segmentation = None):
     else:
         yml["pipeline"]["params"]["segmentation"] = os.path.join(PYANNOTE_DEFAULT_PATH, "pytorch_model.bin")
                                                  
-    # if path_to_embedding:
-    #     yml["pipeline"]["params"]["embedding"] = path_to_embedding
-    # else:
-    #     yml["pipeline"]["params"]["embedding"] = os.path.relpath(
-    #                                                         os.path.join(
-    #                                                         os.path.dirname(__file__),
-    #                                                         "models", "pyannote",
-    #                                                         "speechbrain",
-    #                                                         "spkrec-ecapa-voxceleb",
-    #                                                         "embedding_model.ckpt"))
-    
     if not os.path.exists(yml["pipeline"]["params"]["segmentation"]):
         raise FileNotFoundError(f"Segmentation model not found at {yml['pipeline']['params']['segmentation']}")
-    
-    # if not os.path.exists(yml["pipeline"]["params"]["embedding"]):
-    #     raise FileNotFoundError(f"Embedding model not found at {yml['pipeline']['params']['embedding']}")
     
     with open(file, "w") as stream:
         yaml.dump(yml, stream)
