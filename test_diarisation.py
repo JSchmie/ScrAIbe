2e0989b7e414d22bb6abd21937a65171453f962f
Tryndaron <84569139+Tryndaron@users.noreply.github.com>
2024-01-02 08:22:53 +0100

    Add files via upload
    test file for diarisation functionality

---
+++ test_diarisation.py
@@ -0,0 +1,27 @@
+import pytest
+import os
+from unittest import mock
+from scraibe import Diariser
+
+
+
+@pytest.fixture
+def diariser_instance():
+    with mock.patch.object(Diariser, '_get_token', return_value = 'personal Hugging-Face token')
+        return Diariser('pyannote')
+
+
+
+def test_Diariser_init(diariser_instance):
+    assert diariser_instance.model == 'pyannote'
+
+
+def test_diarisation_function(diariser_instance):
+    with mock.patch.object(diariser_instance.model, 'apply', return_value='diarization_result'):
+        diarization_output = diariser_instance.diarization('example_audio_file.wav')
+        assert diarization_output == 'diarization_result'
+
+
+
+
+
