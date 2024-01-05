fd7e899e4c5978adeaefb124c3fa8d380f931143
Tryndaron <aaron.roggenland@gmx.net>
2024-01-05 15:26:08 +0100

    test2

---
+++ scraibe/test/test_transcriber.py
@@ -0,0 +1,22 @@
+import pytest
+from unittest.mock import patch
+from scraibe import Transcriber
+
+
+
+
+@pytest.mark.parametrize("audio_file, expected_transcription",[("path_to_test_audiofile", "test_transcription")] )
+@patch("scraibe.Transcriber.load_model")
+
+def test_transcriber(mock_load_model, audio_file, expected_transcription):
+    mock_model = mock_load_model.return_value
+    mock_model.transcribe.return_value ={"text": expected_transcription} 
+
+    transcriber = Transcriber.load_model(model="medium")
+
+    transcription_result = transcriber.transcribe(audio=audio_file)
+
+    assert transcription_result == expected_transcription
+
+
+
