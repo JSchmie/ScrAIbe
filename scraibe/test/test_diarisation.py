ffe4dc8c58acebea5fe653c68d6ddc61c167a470
Tryndaron <Aaron.Roggenland@gmx.net>
2024-01-05 12:32:03 +0100

    test

--- scraibe/test/test_diarisation.py
+++ scraibe/test/test_diarisation.py
@@ -16,6 +16,7 @@ def test_Diariser_init(diariser_instance):
     assert diariser_instance.model == 'pyannote'
 
 
+
 def test_diarisation_function(diariser_instance):
     with mock.patch.object(diariser_instance.model, 'apply', return_value='diarization_result'):
         diarization_output = diariser_instance.diarization('example_audio_file.wav')
