033ce8c92ce91b3676c822117031ea9dcb810dc8
Tryndaron <aaron.roggenland@gmx.net>
2024-01-23 13:21:42 +0100

    Diarisation basics tests for the Diarisation object

--- scraibe/test/test_diarisation.py
+++ scraibe/test/test_diarisation.py
@@ -7,26 +7,31 @@ from scraibe import Diariser
 
 @pytest.fixture
 def diariser_instance():
+    """Creates a instance of the Diariser Object for further testing
+
+    Returns:
+        _type_: _description_
+    """
     with mock.patch.object(Diariser, '_get_token', return_value = 'personal Hugging-Face token')
         return Diariser('pyannote')
 
 
 
 def test_Diariser_init(diariser_instance):
-    """_summary_
+    """Tests if the Diariser gets initiated correctly 
 
     Args:
-        diariser_instance (_type_): _description_
+        diariser_instance
     """    
     assert diariser_instance.model == 'pyannote'
 
 
 
 def test_diarisation_function(diariser_instance):
-    """_summary_
+    """tests if the Diariser object with an example audio File 
 
     Args:
-        diariser_instance (_type_): _description_
+        diariser_instance 
     """    
     with mock.patch.object(diariser_instance.model, 'apply', return_value='diarization_result'):
         diarization_output = diariser_instance.diarization('example_audio_file.wav')
