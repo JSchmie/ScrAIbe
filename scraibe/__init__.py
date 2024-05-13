3d0ca8d1e126608fbb8b5f0b25994361b5829646
Schmieder, Jacob <jacob.schmieder@dbfz.de>
2024-05-13 12:51:19 +0000

    removed version file from init

--- scraibe/__init__.py
+++ scraibe/__init__.py
@@ -4,9 +4,7 @@ from .audio import *
 from .transcript_exporter import *
 from .diarisation import *
 
-from .version import get_version as _get_version
 from .misc import *
 
 from .cli import *
  
-__version__ = _get_version()
